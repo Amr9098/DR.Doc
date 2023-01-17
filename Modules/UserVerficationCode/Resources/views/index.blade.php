1cc5af9de46019df12e5bdebf45dfda58028f4e7
Amr abdelfattah <102860949+Amr9098@users.noreply.github.com>
2023-01-17 20:19:32 +0200

    Add User Verification Code
    Signed-off-by: Amr abdelfattah <102860949+Amr9098@users.noreply.github.com>

---
+++ Modules/UserVerficationCode/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('userverficationcode::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('userverficationcode.name') !!}
+    </p>
+@endsection
